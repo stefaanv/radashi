616482de2cb856ef646acc2c36bd2561aca8152c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-03 18:40:32 -0400

    chore: add internal `isTagged` function
    The backbone of many `typed/*` functions. This is not intended for public use.

--- src/mod.ts
+++ src/mod.ts
@@ -108,5 +108,6 @@ export * from './typed/isPrimitive.ts'
 export * from './typed/isPromise.ts'
 export * from './typed/isString.ts'
 export * from './typed/isSymbol.ts'
+export * from './typed/isTagged.ts'
 
 export * from './types'
