e7a77d0f11e0e809c51ac3e2c53103c1f8b54c8c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-07 23:40:38 -0400

    add randomNumber tests

--- tests/random/randomNumber.test.ts
+++ tests/random/randomNumber.test.ts
@@ -1,7 +1,30 @@
 import * as _ from 'radashi'
 
 describe('randomNumber', () => {
-  test('does a thing', () => {
-    expect(_.randomNumber()).toBe(undefined)
+  test('basic functionality', () => {
+    const result = _.randomNumber(1, 10)
+    expect(result).toBeGreaterThanOrEqual(1)
+    expect(result).toBeLessThan(10)
+    const result2 = _.randomNumber(1, 10)
+    expect(result).not.toEqual(result2)
+  })
+  test('should handle negative numbers', () => {
+    const result = _.randomNumber(-10, -1)
+    expect(result).toBeGreaterThanOrEqual(-10)
+    expect(result).toBeLessThanOrEqual(-1)
+  })
+  test('should handle zero', () => {
+    const result = _.randomNumber(0, 0)
+    expect(result).toBe(0)
+  })
+  test('should handle non-integer numbers', () => {
+    const result = _.randomNumber(0.5, 0.6)
+    expect(result).toBeGreaterThanOrEqual(0.5)
+    expect(result).toBeLessThan(0.6)
+  })
+  test('should handle a flipped range', () => {
+    const result = _.randomNumber(10, 1)
+    expect(result).toBeGreaterThanOrEqual(1)
+    expect(result).toBeLessThan(10)
   })
 })
